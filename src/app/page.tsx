1b545ff4b68374ffba85672a91049ddf348af147
vanessasara <127692218+vanessasara@users.noreply.github.com>
2024-11-07 19:07:59 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const Home: React.FC = () => {
 
 
   return (
-    <div>
+    <div className='hidden-x-clip'>
       <Suspense fallback={<Skeleton className="h-64 w-full" />}>
         <Slider />
       </Suspense>
